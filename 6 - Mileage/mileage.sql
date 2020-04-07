4ffdd5af96f9a9f0727f6323a251b2b4fed00f4d
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-04-07 19:25:35 -0400

    Update mileage.sql

--- 6 - Mileage/mileage.sql
+++ 6 - Mileage/mileage.sql
@@ -59,8 +59,8 @@ group by vehicle_id;
 -- 11
 select 
 case
-	when vehicle_id = 2 then 'Prius'
-	when vehicle_id <> 2 then 'Other'
+	when round(avg(mpg)) = 39 then 'Prius'
+	when round(avg(mpg)) <> 39 then 'Other'
 end as 'vehicle_id', 
 round(avg(mpg)) as avg_mpg from fillups
 where mpg <> 0
@@ -72,3 +72,43 @@ count(hour(date_time)) as num_times
 from fillups 
 group by hour 
 order by hour;
+
+-- 13
+select 
+case 
+	when dayofweek(date_time) = 1 then 'Sun'
+	when dayofweek(date_time) = 2 then 'Mon'
+	when dayofweek(date_time) = 3 then 'Tue'
+	when dayofweek(date_time) = 4 then 'Wed'
+	when dayofweek(date_time) = 5 then 'Thu'
+	when dayofweek(date_time) = 6 then 'Fri'
+	when dayofweek(date_time) = 7 then 'Sat'
+end as 'day',
+count(dayofweek(date_time)) as num_fillups
+from fillups
+group by day
+order by num_fillups desc;
+
+-- 14
+select 
+case 
+	when month(date_time) = 1 then 'Jan'
+	when month(date_time) = 2 then 'Feb'
+	when month(date_time) = 3 then 'Mar'
+	when month(date_time) = 4 then 'Apr'
+	when month(date_time) = 5 then 'May'
+	when month(date_time) = 6 then 'Jun'
+	when month(date_time) = 7 then 'Jul'
+	when month(date_time) = 8 then 'Aug'
+	when month(date_time) = 9 then 'Sep'
+	when month(date_time) = 10 then 'Oct'
+	when month(date_time) = 11 then 'Nov'
+	when month(date_time) = 12 then 'Dec'
+end as 'month',
+left(date_time, 4) as year,
+vehicle_id,
+count(month(date_time)) as nbr_fillups,
+round(volume * count(month(date_time))) as total_gallons
+from fillups
+group by year, month(date_time)
+order by total_gallons desc;
