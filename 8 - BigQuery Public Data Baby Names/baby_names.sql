fc82511bd22aea15578ef8cfb2d6e6db6295e425
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-04-21 23:36:24 -0400

    Update baby_names.sql

--- 8 - BigQuery Public Data Baby Names/baby_names.sql
+++ 8 - BigQuery Public Data Baby Names/baby_names.sql
@@ -2,7 +2,18 @@
 SELECT
   count(*) as record_count
 FROM
+  `bigquery-public-data.usa_names.usa_1910_current`;
+
+-- Q2
+SELECT
+  gender,
+  sum(number) as total_births
+FROM
   `bigquery-public-data.usa_names.usa_1910_current`
+GROUP BY
+  gender
+ORDER BY
+  total_births desc;
 
 -- Q3
 SELECT
@@ -26,3 +37,29 @@ GROUP BY
   name, state
 HAVING
   max(number) = 10021 and state = 'NY';
+
+-- Q5
+SELECT
+  name,
+  sum(number) as total
+FROM
+  `bigquery-public-data.usa_names.usa_1910_current`
+WHERE
+  name in (
+    SELECT
+      name
+    FROM
+      `bigquery-public-data.usa_names.usa_1910_current`
+    WHERE
+      state = 'FL'
+    GROUP BY 
+      name, state
+    ORDER BY
+      sum(number) desc
+    LIMIT 1
+  )
+GROUP BY
+  name
+ORDER BY
+  total desc
+LIMIT 1;
